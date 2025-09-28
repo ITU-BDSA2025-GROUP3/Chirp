f85fc42a6c8fc956f4ac049c29df5f3f523f70a5
Y23n <yasn@itu.dk>
2025-09-28 22:08:45 +0200

    Not finish, I need to work on another branch

---
+++ test/SimpleDB.Tests/UnitTestCSVRead.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleDB.Tests;
+
+public class UnitTestCSVRead
+{
+    // theory // skriv det den skal teste med i
+    // fact // enkel test
+    [Fact]
+    public void TestRead()
+    {
+        
+    }
+}
