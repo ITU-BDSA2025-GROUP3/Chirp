46765a7f67eba5ed38285c414a5dc0659b0b886f
DanishMathMan <120851542+DanishMathMan@users.noreply.github.com>
2025-09-29 17:48:18 +0200

    test:fix: Fixed issue with testing framework
    Fixed an issue with project not being able to run tests, by deleting it and adding it again via Rider's method of adding a new project.

---
+++ test/Chirp.Razor.Tests/TestAPI.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+using Xunit;
+
+public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _fixture;
+    private readonly HttpClient _client;
+
+    public TestAPI(WebApplicationFactory<Program> fixture)
+    {
+        _fixture = fixture;
+        _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
+    }
+
+    [Fact]
+    public async Task Test1()
+    {
+        Assert.True(true);
+    }
+}
