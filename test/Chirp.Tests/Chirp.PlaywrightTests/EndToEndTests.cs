97110beff7a8b1906605af0e81c308f72499e590
Elisa Esgici <elie@itu.dk>
2025-11-06 21:07:42 +0100

    Create EndToEndTests.cs

---
+++ test/Chirp.Tests/Chirp.PlaywrightTests/EndToEndTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace Chirp.Tests.Chirp.PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class LoginTests : PageTest
+{
+   
+}
