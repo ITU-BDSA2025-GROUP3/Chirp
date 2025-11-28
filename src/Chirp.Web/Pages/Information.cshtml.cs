3d80e86b3fd1eb4f133b4d7ff595723c6c2de01b
Y23n <yasn@itu.dk>
2025-11-28 12:51:13 +0100

    Work in progress
    The UI is done but there are some flaws with the data that the information page that needs to be fixed

--- src/Chirp.Web/Pages/Information.cshtml.cs
+++ src/Chirp.Web/Pages/Information.cshtml.cs
@@ -1,17 +1,65 @@
+using System.Text;
+using Chirp.Core;
 using Chirp.Core.ServiceInterfaces;
 
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Chirp.Web.Pages;
 
-public class InformationModel(IAuthorService authorService, ICheepService cheepService) : PageModel
+ [Authorize]
+public class InformationModel : PageModel
 {
-    public required List<CheepDTO> Cheeps { get; set; }
+    private readonly ICheepService _cheepService;
+    private readonly IAuthorService _authorService;
+
+    public InformationModel(ICheepService cheepService, IAuthorService authorService)
+    {
+        _cheepService = cheepService;
+        _authorService = authorService;
+    }
     
-    // Show name
-    // email
+    
+    public AuthorDTO? CurrentAuthor { get; set; }
+    public List<CheepDTO> Cheeps { get; set; }
+    
+    public List<AuthorDTO> Followers { get; set; } = new();
+
+    public async Task OnGetAsync()
+    {
+        await getUserNameAndEmail();
+        await getFollowList();
+        await getCheepsList();
+    }
+
+    public async Task getUserNameAndEmail()
+    {
+        var userName = HttpContext.User.Identity.Name;
+       // CurrentAuthor = await _authorService.GetAuthor(userName);
+    }
+    
     // link to other user this user is fowllowing
+    public async Task getFollowList()
+    {
+        var userName = HttpContext.User.Identity.Name;
+        var list = await _authorService.GetFollowsList(userName);
+        Followers = list.ToList();
+    }
+
     // all cheeps of the user should be listed
-    // download button
+    public async Task getCheepsList()
+    {
+        var userName = HttpContext.User.Identity.Name;
+        var cheeps = await _authorService.GetAuthorCheeps(userName);
+        Cheeps = cheeps.ToList();
+    }
     
+    // optional
+    /*public async Task<IActionResult> OnGetDownloadAsync()
+    {
+        return await Task.FromResult<IActionResult>(Page());
+    }*/
 }
+
