ae77d79b25963a87154923e2c1863802f2d04ead
Alexandra <alyp@itu.dk>
2025-11-30 13:42:39 +0100

    fixed bug page reload to page 1 when commenting

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -17,55 +17,59 @@ public class PublicModel : PageModel
         _authorService = authorService;
         _commentService = commentService;
     }
+
     public required List<CheepDTO> Cheeps { get; set; }
     public List<AuthorDTO> Followers { get; private set; } = new();
     public List<CommentDTO> Comments { get; set; } = new();
     public int TotalPages { get; private set; }
     public int CurrentPage;
+    private async Task LoadCheeps(int page)
+    {
+        _cheepService.CurrentPage = page;
+        Cheeps = await _cheepService.GetCheeps();
+        Followers = await _authorService.GetFollowsList(User.Identity!.Name!);
+        Comments = await _commentService.GetComments();
+        TotalPages = await _cheepService.GetTotalCheeps();
+        CurrentPage = page;
+    }
     
     [BindProperty]
     [Required(ErrorMessage = "Please enter a Cheep!")]
     [StringLength(160, ErrorMessage = "Cheeps cannot exceed 160 characters.")]
     public string Message { get; set; } = string.Empty;
-    public async Task<ActionResult> OnPostCheepAsync()
+    public async Task<ActionResult> OnPostCheepAsync(int page)
     {
         ModelState.Remove(nameof(Comment));
         var author = User.Identity!.Name;
         if (!ModelState.IsValid)
         {
-            await LoadCheeps();
+            await LoadCheeps(page);
             return Page();
         }
         await _cheepService.AddNewCheep(author!, Message);
-        return RedirectToPage();
-    }
-    private async Task LoadCheeps()
-    {
-        _cheepService.CurrentPage = 1;
-        Cheeps = await _cheepService.GetCheeps();
-        Followers = await _authorService.GetFollowsList(User.Identity!.Name!);
-        Comments = await _commentService.GetComments();
-        TotalPages = await _cheepService.GetTotalCheeps();
-        CurrentPage = _cheepService.CurrentPage;
+        await LoadCheeps(page);
+        return Page();
     }
     
-    public async Task<ActionResult> OnPostFollowAsync(string authorToFollow)
+    public async Task<ActionResult> OnPostFollowAsync(string authorToFollow, int page)
     {
         var follower = User.Identity!.Name;
         await _authorService.AddAuthorToFollowsList(authorToFollow, follower!);
         
         var follows = await _authorService.GetFollowsList(follower!);
         Console.WriteLine($"[{DateTime.Now}] {follower} now follows: {string.Join(",", follows.Select(a => a.Name))}");
-        return RedirectToPage();
+        await LoadCheeps(page);
+        return Page();
     }
     
-    public async Task<ActionResult> OnPostUnfollowAsync(string authorToUnfollow)
+    public async Task<ActionResult> OnPostUnfollowAsync(string authorToUnfollow, int page)
     {
         var follower = User.Identity!.Name;
         await _authorService.RemoveAuthorFromFollowsList(authorToUnfollow, follower!);
         
         Console.WriteLine($"[{DateTime.Now}] {follower} now unfollows: {authorToUnfollow}");
-        return RedirectToPage();
+        await LoadCheeps(page);
+        return Page();
     }
     
     [BindProperty] 
@@ -75,27 +79,27 @@ public class PublicModel : PageModel
     [StringLength(160, ErrorMessage = "Comments cannot exceed 160 characters.")]
     public string Comment { get; set; } = string.Empty;
     
-    public async Task<ActionResult> OnPostCommentFormAsync(int cheepId)
+    public async Task<ActionResult> OnPostCommentFormAsync(int cheepId, int page)
     {
         ModelState.Remove(nameof(Message));
         if (!ModelState.IsValid)
         {
-            await LoadCheeps();
+            await LoadCheeps(page);
             return Page();
         }
 
         CommentTargetId = cheepId;
         ModelState.Clear();
         await _commentService.AddNewComment(User.Identity!.Name!, Comment, cheepId);
-        await LoadCheeps();
+        await LoadCheeps(page);
         return Page();
     }
     
-    public async Task<ActionResult> OnPostToggleCommentsAsync(int cheepId)
+    public async Task<ActionResult> OnPostToggleCommentsAsync(int cheepId, int page)
     {
         CommentTargetId = CommentTargetId == cheepId ? 0 : cheepId;
         ModelState.Clear();
-        await LoadCheeps();
+        await LoadCheeps(page);
         return Page();
     }
 
@@ -108,8 +112,8 @@ public class PublicModel : PageModel
             
             _cheepService.CurrentPage = pageQuery;
             Cheeps = await _cheepService.GetCheeps();
-            Comments = await _commentService.GetComments();
             Followers = await _authorService.GetFollowsList(User.Identity!.Name!);
+            Comments = await _commentService.GetComments();
             TotalPages = await _cheepService.GetTotalCheeps();
             CurrentPage = pageQuery;
         }
